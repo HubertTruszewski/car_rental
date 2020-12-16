654b2469e4fbdc76fc64dc5cfa37f0d1477018bb
Hubert Truszewski <01150052@pw.edu.pl>
2020-12-16 16:30:58 +0100

    Added body to auto_menu()

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
+import os
+
+
+def clear_terminal():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+
 def print_logo():
     print("""
    #    #     # ####### ####### #     #
@@ -19,10 +26,40 @@ def print_main_menu():
     print("1. Auta")
     print("2. Rezerwacje")
     print("3. Wypożyczenia")
+    print("0. Wyjście z programu")
+
+
+def add_car():
+    pass
+
+
+def manipulate_car():
+    pass
 
 
 def auto_menu():
-    pass
+    print("MENU - AUTA")
+    print("Proszę wybrać jedną z dostępnych opcji:")
+    print("1. Dodanie nowego samochodu")
+    print("2. Wyszukiwanie pojazdu, edycja danych, usuwanie")
+    print("9. Powrót do głównego menu")
+
+    answer = 0
+
+    while not answer:
+        answer = int(input("Wybór: "))
+    if answer == 1:
+        clear_terminal()
+        add_car()
+    elif answer == 2:
+        clear_terminal()
+        manipulate_car()
+    elif answer == 9:
+        clear_terminal()
+        main()
+    else:
+        print("Niepoprawny wybór, spróbuj jeszcze raz:")
+        answer = 0
 
 
 def reservation_menu():
@@ -43,11 +80,16 @@ def main():
     while not answer:
         answer = int(input("Wybór: "))
         if answer == 1:
+            clear_terminal()
             auto_menu()
         elif answer == 2:
+            clear_terminal()
             reservation_menu()
         elif answer == 3:
+            clear_terminal()
             rental_menu()
+        elif answer == 0:
+            break
         else:
             print("Niepoprawny wybór, spróbuj jeszcze raz:")
             answer = 0
