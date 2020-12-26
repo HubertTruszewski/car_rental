a1c8fc9875e7d0b55de93c20b4b981cfed83e59b
Hubert Truszewski <01150052@pw.edu.pl>
2020-12-26 19:19:01 +0100

    Added getters to classes Car, PassengerCar, Van

--- classes.py
+++ classes.py
@@ -12,6 +12,33 @@ class Car:
         self._price = price
         self._type_id = 0
 
+    def db_id(self):
+        return self._db_id
+
+    def mark(self):
+        return self._mark
+
+    def model(self):
+        return self._model
+
+    def registration_number(self):
+        return self._registration_number
+
+    def fuel_consumption(self):
+        return self._fuel_consumption
+
+    def doors(self):
+        return self._doors
+
+    def color(self):
+        return self._color
+
+    def price(self):
+        return self._price
+
+    def type_id(self):
+        return self._type_id
+
 
 class PassengerCar(Car):
     def __init__(self, mark, model, registration_number, seats,
@@ -24,6 +51,15 @@ class PassengerCar(Car):
         self._isofix = bool(isofix)
         self._type_id = 1
 
+    def body(self):
+        return self._body
+
+    def classification(self):
+        return self._classification
+
+    def isofix(self):
+        return self._isofix
+
 
 class Van(Car):
     def __init__(self, mark, model, registration_number, seats,
@@ -32,5 +68,11 @@ class Van(Car):
         super().__init__(mark, model, registration_number, seats,
                          fuel_consumption, doors, color, price, db_id)
         self._capacity = capacity
-        self._side_door = side_door
+        self._side_door = bool(side_door)
         self._type_id = 2
+
+    def capacity(self):
+        return self._capacity
+
+    def side_door(self):
+        return self._side_door
